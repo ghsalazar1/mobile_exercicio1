446f21c7ebf9084c9387dbdb58261a0ed6174fce
Gleybe Hebson Salazar Cavalcanti <gleybe.cavalcanti@vectracs.com.br>
2023-02-16 15:43:18 -0300

    [DEV] - Alterando título da aplicação

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,6 @@
 /*!
-    * Start Bootstrap - SB Admin v7.0.5 (https://startbootstrap.com/template/sb-admin)
-    * Copyright 2013-2022 Start Bootstrap
+    * Exercício 1 - Mobile - SB Admin v7.0.5 (https://startbootstrap.com/template/sb-admin)
+    * Copyright 2013-2022 Exercício 1 - Mobile
     * Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-sb-admin/blob/master/LICENSE)
     */
     // 
